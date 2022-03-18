b511f45d71b19b1e6fc346fde22a9db239851361
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-18 18:53:46 -0500

    solved problems in length error lentgh

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Gasto from './Gasto'
 
 const ListadoGastos = ({ 
@@ -8,16 +7,11 @@ const ListadoGastos = ({
   filtro,
   gastosFiltrados 
 }) => {
-  console.log(gastos)
-  setTimeout(() => {
-    console.log(gastos.lentgh)
-  }, 3000);
   return (
     <div className='listado-gastos contenedor'>
-      <h2>{ gastos.lentgh ? 'Gastos' : 'No Hay Gastos' }</h2>
       { filtro ? (
           <>
-            {/* <h2>{ gastosFiltrados.lentgh ? 'Gastos' : 'No Hay Gastos' }</h2> */}
+            <h2>{ gastosFiltrados.length ? 'Gastos' : 'No Hay Gastos' }</h2>
             { gastosFiltrados.map(gasto => (
               <Gasto 
                 key={gasto.id}
@@ -29,7 +23,7 @@ const ListadoGastos = ({
           </>
         ) : (
           <>
-            {/* <h2>{ gastos.lentgh ? 'Gastos' : 'No Hay Gastos' }</h2> */}
+            <h2>{ gastos.length ? 'Gastos' : 'No Hay Gastos' }</h2>
             { gastos.map(gasto => (
               <Gasto 
                 key={gasto.id}
